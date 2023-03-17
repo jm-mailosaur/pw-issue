9a70919a2656e9b5c3c80cce16bc2e1b9f6d9588
Jon M <jon@mailosaur.com>
2023-03-17 17:29:20 +0000

    Initial commit

---
+++ src/App.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { test, expect } from '@playwright/experimental-ct-react';
+
+test.use({ viewport: { width: 500, height: 500 } });
+
+test('should work', async ({ mount }) => {
+    const component = await mount(<div>Hello world</div>);
+    await expect(component).toContainText('Hello world');
+});
